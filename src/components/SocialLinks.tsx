624d617d87c9fe735cdce8b4a6aff5b07042fc2f
black99hard <yasser99hard@gmail.com>
2024-12-16 22:53:41 +0100

    d

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -7,7 +7,8 @@ const SocialLinks: React.FC = () => {
     { name: 'Telegram', url: 'https://t.me/emptinesscto', icon: Send },
     { name: 'X', url: 'https://x.com/EmptinessCTO', icon: Twitter },
     { name: 'Dev', url: 'https://t.me/Yakshustles', icon: Code },
-    { name: 'Infinite Backrooms', url: 'https://www.infinitebackrooms.com/dreams/conversation-1713232205-scenario-vanilla-backrooms-txt', icon: Terminal }
+    { name: 'Infinite Backrooms', url: 'https://www.infinitebackrooms.com/dreams/conversation-1713232205-scenario-vanilla-backrooms-txt', icon: Terminal },
+    { name: 'DEX', url: 'https://dexscreener.com/solana/ebczzgmdpjjryfyn5hmb7xclrxnafb9knatxaaspzmmm' }
   ];
 
   return (
@@ -20,7 +21,7 @@ const SocialLinks: React.FC = () => {
         rel="noopener noreferrer"
         className="flex items-center gap-2 px-4 py-2 bg-gray-900/50 rounded-lg border border-gray-800 hover:bg-gray-800/50 transition-colors"
       >
-        <link.icon className="w-4 h-4" />
+        {link.icon ? <link.icon className="w-4 h-4" /> : <span>{link.name}</span>}
         {link.name === 'Dev' && <span>{link.name}</span>}
       </a>
       ))}
